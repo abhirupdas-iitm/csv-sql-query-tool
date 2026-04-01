39c6170da0c4f14f6c08151cc38bb7ff77b8f6c8
Abhirup Das <23f2003574@ds.study.iitm.ac.in>
2026-04-01 15:53:20 +0530

    added the proper config for reference by auth.js

--- frontend/auth.js
+++ frontend/auth.js
@@ -8,11 +8,6 @@ import {
     onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDfqzhtRbh25ITYaTN9W0kA9VHfvj1rG5Y",
